c1755597525cb4cf20ea560f0b48c1b4eb7c9901
AVMUser-55757928@LODSPRODMCA.onmicrosoft.com <AVMUser-55757928@LODSPRODMCA.onmicrosoft.com>
2025-10-21 12:14:16 -0700

    import resources not created by terraform

--- main.tf
+++ main.tf
@@ -18,3 +18,27 @@ resource "azurerm_resource_group" "demo" {
   location = var.region
   tags     = var.tags
 }
+
+locals {
+  subnets = cidrsubnets("10.0.0.0/24", 8, 7)
+}
+
+resource "azurerm_resource_group" "temp_resource_group_1" {
+  name     = "temp_rg1"
+  location = "UK South"
+
+  tags = {
+    reference = data.azurerm_resource_group.temp_resource_group_2.id
+  }
+}
+
+data "azurerm_resource_group" "temp_resource_group_2" {
+  name = "temp_rg2"
+}
+
+data "azurerm_client_config" "current" {}
+
+import {
+  to = azurerm_resource_group.temp_resource_group_1
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/temp_rg1"
+}
