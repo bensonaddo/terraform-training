7d8e080c1736a6037eab87a0e6d2ec5e637ce0ff
AVMUser-55757928@LODSPRODMCA.onmicrosoft.com <AVMUser-55757928@LODSPRODMCA.onmicrosoft.com>
2025-10-21 12:25:34 -0700

    remove import scripts

--- main.tf
+++ main.tf
@@ -12,33 +12,13 @@ provider "azurerm" {
   resource_provider_registrations = "core"
 }
 
+locals {
+  subnets = cidrsubnets("10.0.0.0/24", 6, 7, 8)
+}
+
 resource "azurerm_resource_group" "demo" {
   for_each = var.resource_groups
   name     = "${var.prefix}_${each.value}"
   location = var.region
   tags     = var.tags
-}
-
-locals {
-  subnets = cidrsubnets("10.0.0.0/24", 8, 7)
-}
-
-resource "azurerm_resource_group" "temp_resource_group_1" {
-  name     = "temp_rg1"
-  location = "UK South"
-
-  tags = {
-    reference = data.azurerm_resource_group.temp_resource_group_2.id
-  }
-}
-
-data "azurerm_resource_group" "temp_resource_group_2" {
-  name = "temp_rg2"
-}
-
-data "azurerm_client_config" "current" {}
-
-import {
-  to = azurerm_resource_group.temp_resource_group_1
-  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/temp_rg1"
 }
